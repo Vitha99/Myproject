b01ba35c20605759a90b631862e233db8b2b0cb9
Vitha99 <134637267+Vitha99@users.noreply.github.com>
2023-10-15 20:02:07 +0530

    Update Vitha.py

--- Vitha.py
+++ Vitha.py
@@ -1,2 +1,9 @@
 print("Hello, World!")
 print("Vithakshana")
+
+name = "Vithakshana"
+age = 24
+height = 5.11
+is_student = True
+
+print(f"My name is {name}, I am {age} years old, {height} feet tall, and I am a student: {is_student}")
